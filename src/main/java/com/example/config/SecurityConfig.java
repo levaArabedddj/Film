8744c7fa96090e2fcce2d75affd7dfd792344568
levaArabedddj <127316849+levaArabedddj@users.noreply.github.com>
2025-05-21 20:54:16 +0200

    Update SecurityConfig.java

--- src/main/java/com/example/config/SecurityConfig.java
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -45,7 +45,13 @@ import org.springframework.web.filter.CorsFilter;
 
 import java.io.*;
 import java.util.List;
-
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 
 
@@ -122,6 +128,24 @@ public class SecurityConfig {
         source.registerCorsConfiguration("/**", corsConfig);
         return source;
     }
+
+    @Bean
+public FilterRegistrationBean<CorsFilter> corsFilterRegistration() {
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowedOrigins(List.of("https://mellow-dolphin-38542e.netlify.app"));
+    config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
+    config.setAllowedHeaders(List.of("*"));
+    config.setAllowCredentials(true);
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", config);
+
+    FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+    bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    return bean;
+}
+
+    
     @Bean
     public CorsFilter corsFilter() {
         return new CorsFilter(corsConfigurationSource()); // Используем правильный CorsFilter
